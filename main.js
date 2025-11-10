72d35bea7c61fb62e35ca1b76fb6b17e7209d7b2
Your Name <you@example.com>
2025-11-10 23:46:26 +0900

    トークン入れ忘れ

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // ==== Ionトークン ====
-Cesium.Ion.defaultAccessToken = "＜あなたのトークン＞";
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyOGRiZmY3Yy0wNzRjLTQ2MjktOGQ0Ni0xYmI5MzFmNDUxZDAiLCJpZCI6MzU0MDY0LCJpYXQiOjE3NjE0NTQ3MDh9.p9q4yTuNNbVz7U09nx04n-LQG0sxXh8TDw22H3FSIV0";
 
 import { initViewer } from "./common/baseViewer.js";
 import { loadLayers } from "./common/layers.js";
