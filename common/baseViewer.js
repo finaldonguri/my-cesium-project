9e2b2114e92cf3abf7fe7ade2fb3151faaf62517
Your Name <you@example.com>
2025-11-10 22:55:19 +0900

    新しい書き方なら terrain: に Cesium.Terrain.fromWorldTerrain() を渡します

--- common/baseViewer.js
+++ common/baseViewer.js
@@ -1,7 +1,5 @@
 // /common/baseViewer.js
-export function initViewer({ ionToken } = {}) {
-    if (ionToken) Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyOGRiZmY3Yy0wNzRjLTQ2MjktOGQ0Ni0xYmI5MzFmNDUxZDAiLCJpZCI6MzU0MDY0LCJpYXQiOjE3NjE0NTQ3MDh9.p9q4yTuNNbVz7U09nx04n-LQG0sxXh8TDw22H3FSIV0";
-
+export function initViewer(opts = {}) {
     const viewer = new Cesium.Viewer("cesiumContainer", {
         baseLayerPicker: false,
         timeline: false,
@@ -10,7 +8,7 @@ export function initViewer({ ionToken } = {}) {
         homeButton: false,
         sceneModePicker: false,
         navigationHelpButton: false,
-        terrainProvider: Cesium.createWorldTerrain(),
+        terrain: Cesium.Terrain.fromWorldTerrain(), 
     });
 
     // スマホ向け：ピクセル比を抑えて負荷軽減（必要に応じて）
